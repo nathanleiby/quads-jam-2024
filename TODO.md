eb5790029a6104c2a98edebccd5f1e80e9d8e754
Nathan Leiby <nathanleiby@gmail.com>
2024-10-21 13:46:59 -0400

    refactor

--- TODO.md
+++ TODO.md
@@ -13,3 +13,18 @@ high level: implement lighting approach described here:
 - draw triangles for shadow and light
   - `draw_triangle(v1, v2, v3, color)`
 - light influence fades with distance from source (see redblob demo)
+
+GAMEPLAY
+
+- implement asteroids
+  - [ ] movement (momentum, wrap around)
+  - [ ] floating asteroids
+  - [ ] collisions (hit something and take damage + blow it up + invuln briefly)
+- make it a game!
+  - add collecting "warp crystals" to make a warp, which completes a level.
+  - 3-5 levels of increasing difficulty. About 30s-1m each.
+    - (# asteroids, size of asteroid).
+    - future: could be lots of other things.
+  - add game win condition once all levels complete.
+  - add game lose condition, when you die in a given level.
+- add juice! SFX, music, lights, particles
--- src/scene/gameplay.rs
+++ src/scene/gameplay.rs
@@ -11,24 +11,18 @@ use crate::input::action_pressed;
 use crate::input::Action;
 use crate::math::Ray;
 use crate::math::Segment;
-use macroquad::color::BLUE;
-use macroquad::color::GREEN;
-use macroquad::color::ORANGE;
-use macroquad::color::RED;
-use macroquad::color::WHITE;
-use macroquad::math::Circle;
+use macroquad::color::*;
 use macroquad::math::Vec2;
-use macroquad::miniquad::window::screen_size;
+use macroquad::rand;
 use macroquad::shapes::draw_circle;
 use macroquad::shapes::draw_line;
-use macroquad::shapes::draw_triangle;
 use macroquad::time::get_frame_time;
 
 pub struct Gameplay {
     pause_subscene: Pause,
 
     player_position: Vec2,
-    // segments: Vec<Segment>,
+    walls: Vec<Segment>,
 }
 
 const MOVEMENT_SPEED: f32 = 300.;
@@ -43,6 +37,151 @@ impl Scene for Gameplay {
             play_sfx(ctx, &ctx.audio.sfx.menu_select);
         }
 
+        self._player_movement(ctx);
+    }
+
+    fn draw(&mut self, ctx: &mut Context) {
+        if self.pause_subscene.active {
+            self.pause_subscene.draw(ctx);
+        } else {
+            self._draw_scene();
+        }
+    }
+}
+
+impl Gameplay {
+    pub async fn new(ctx: &mut Context) -> Self {
+        let pause_subscene = Pause::new(ctx);
+        let player_position = Vec2::new(300., 300.);
+
+        let mut walls = vec![
+            // outer walls
+            Segment {
+                src: Vec2::new(0., 0.),
+                dst: Vec2::new(0., VIRTUAL_HEIGHT),
+            },
+            Segment {
+                src: Vec2::new(0., 0.),
+                dst: Vec2::new(VIRTUAL_WIDTH, 0.),
+            },
+            Segment {
+                src: Vec2::new(VIRTUAL_WIDTH, 0.),
+                dst: Vec2::new(VIRTUAL_WIDTH, VIRTUAL_HEIGHT),
+            },
+            Segment {
+                src: Vec2::new(0., VIRTUAL_HEIGHT),
+                dst: Vec2::new(VIRTUAL_WIDTH, VIRTUAL_HEIGHT),
+            },
+            // inner obstacles
+            Segment {
+                src: Vec2::new(0., 100.),
+                dst: Vec2::new(100., 100.),
+            },
+            Segment {
+                src: Vec2::new(100., 0.),
+                dst: Vec2::new(100., 100.),
+            },
+            Segment {
+                src: Vec2::new(300., 100.),
+                dst: Vec2::new(400., 100.),
+            },
+            Segment {
+                src: Vec2::new(400., 300.),
+                dst: Vec2::new(450., 400.),
+            },
+            Segment {
+                src: Vec2::new(500., 300.),
+                dst: Vec2::new(590., 200.),
+            },
+        ];
+
+        // TODO: How to seed? It's giving same results every time.
+        let gen_uniform = || rand::rand() as f32 / u32::MAX as f32;
+        for _ in 0..5 {
+            walls.push(Segment {
+                src: Vec2::new(
+                    gen_uniform() * VIRTUAL_WIDTH,
+                    gen_uniform() * VIRTUAL_HEIGHT,
+                ),
+                dst: Vec2::new(
+                    gen_uniform() * VIRTUAL_WIDTH,
+                    gen_uniform() * VIRTUAL_HEIGHT,
+                ),
+            })
+        }
+
+        Self {
+            pause_subscene,
+            player_position,
+            walls,
+        }
+    }
+
+    fn _draw_scene(&mut self) {
+        // Draw light source
+        let source = self.player_position;
+        draw_circle(source.x, source.y, 10., WHITE);
+        // draw_texture(&ctx.textures.example, 400., 300., WHITE);
+        // Draw lines from light source to corners of squares
+
+        // Create rays, sweeping over 360 degrees
+        // TODO: Replace with only rays pointed at each intersection point in scene
+        let mut rays = vec![];
+        let num_rays = 360;
+        for idx in 0..num_rays {
+            let ratio = idx as f32 / num_rays as f32;
+            let angle = ratio * 2. * PI;
+            rays.push(Ray {
+                origin: source,
+                dir: Vec2::new(angle.cos(), angle.sin()),
+            })
+        }
+
+        // draw walls
+        for w in &self.walls {
+            draw_line(w.src.x, w.src.y, w.dst.x, w.dst.y, 4., BLUE);
+        }
+
+        // find intersections
+        let mut intersections = vec![];
+        for ray in &rays {
+            let mut closest_intersection: Option<Vec2> = None;
+            for w in &self.walls {
+                if let Some(new_intersection) = ray.intersection(w) {
+                    if let Some(existing_int) = closest_intersection {
+                        if ray.origin.distance(existing_int)
+                            >= ray.origin.distance(new_intersection)
+                        {
+                            closest_intersection = Some(new_intersection);
+                        }
+                    } else {
+                        closest_intersection = Some(new_intersection);
+                    }
+                }
+            }
+
+            // only keep the nearest one
+            if let Some(intersection) = closest_intersection {
+                intersections.push(intersection);
+            }
+        }
+
+        // draw rays and interactions
+        for intersection in intersections {
+            draw_line(
+                source.x,
+                source.y,
+                intersection.x,
+                intersection.y,
+                1.,
+                // WHITE,
+                Color::new(1.00, 1.00, 1.00, 0.5),
+            );
+            draw_circle(intersection.x, intersection.y, 2., ORANGE);
+        }
+    }
+
+    fn _player_movement(&mut self, ctx: &mut Context) {
         let mut movement_vec = Vec2::new(0., 0.);
         if action_down(Action::Up, &ctx.gamepads) {
             movement_vec.y += -1.;
@@ -63,117 +202,6 @@ impl Scene for Gameplay {
             self.player_position = self
                 .player_position
                 .clamp(Vec2::default(), Vec2::new(VIRTUAL_WIDTH, VIRTUAL_HEIGHT));
-        }
-    }
-
-    fn draw(&mut self, ctx: &mut Context) {
-        if self.pause_subscene.active {
-            self.pause_subscene.draw(ctx);
-        } else {
-            // Draw light source
-            let source = self.player_position;
-            draw_circle(source.x, source.y, 10., WHITE);
-            // draw_texture(&ctx.textures.example, 400., 300., WHITE);
-            // Draw lines from light source to corners of squares
-
-            // Create rays, sweeping over 360 degrees
-            // TODO: Replace with only rays pointed at each intersection point in scene
-            let mut rays = vec![];
-            let num_rays = 36;
-            for idx in 0..num_rays {
-                let ratio = idx as f32 / num_rays as f32;
-                let angle = ratio * 2. * PI;
-                rays.push(Ray {
-                    origin: source,
-                    dir: Vec2::new(angle.cos(), angle.sin()),
-                })
-            }
-
-            let walls = vec![
-                // outer walls
-                Segment {
-                    src: Vec2::new(0., 0.),
-                    dst: Vec2::new(0., VIRTUAL_HEIGHT),
-                },
-                Segment {
-                    src: Vec2::new(0., 0.),
-                    dst: Vec2::new(VIRTUAL_WIDTH, 0.),
-                },
-                Segment {
-                    src: Vec2::new(VIRTUAL_WIDTH, 0.),
-                    dst: Vec2::new(VIRTUAL_WIDTH, VIRTUAL_HEIGHT),
-                },
-                Segment {
-                    src: Vec2::new(0., VIRTUAL_HEIGHT),
-                    dst: Vec2::new(VIRTUAL_WIDTH, VIRTUAL_HEIGHT),
-                },
-                // inner obstacles
-                Segment {
-                    src: Vec2::new(0., 100.),
-                    dst: Vec2::new(100., 100.),
-                },
-                Segment {
-                    src: Vec2::new(100., 0.),
-                    dst: Vec2::new(100., 100.),
-                },
-                Segment {
-                    src: Vec2::new(300., 100.),
-                    dst: Vec2::new(400., 100.),
-                },
-            ];
-
-            // draw walls
-            for w in &walls {
-                draw_line(w.src.x, w.src.y, w.dst.x, w.dst.y, 4., BLUE);
-            }
-
-            // find intersections
-            let mut intersections = vec![];
-            for ray in &rays {
-                let mut closest_intersection: Option<Vec2> = None;
-                for w in &walls {
-                    if let Some(new_intersection) = ray.intersection(w) {
-                        if let Some(existing_int) = closest_intersection {
-                            if ray.origin.distance(existing_int)
-                                >= ray.origin.distance(new_intersection)
-                            {
-                                closest_intersection = Some(new_intersection);
-                            }
-                        } else {
-                            closest_intersection = Some(new_intersection);
-                        }
-                    }
-                }
-
-                // only keep the nearest one
-                if let Some(intersection) = closest_intersection {
-                    intersections.push(intersection);
-                }
-            }
-
-            // TODO: prune to only keep the closest intersections
-            // TODO: Move all the math outside, to clearly separate it from drawing logic
-
-            // draw rays and interactions
-            for intersection in intersections {
-                draw_line(source.x, source.y, intersection.x, intersection.y, 1., BLUE);
-                draw_circle(intersection.x, intersection.y, 2., ORANGE);
-            }
-
-            draw_line(source.x, source.y, 100., 0., 1., RED);
-            draw_line(source.x, source.y, 100., 100., 1., RED);
-            draw_line(source.x, source.y, 300., 100., 1., RED);
-        }
-    }
-}
-
-impl Gameplay {
-    pub async fn new(ctx: &mut Context) -> Self {
-        let pause_subscene = Pause::new(ctx);
-        let player_position = Vec2::new(300., 300.);
-        Self {
-            pause_subscene,
-            player_position,
         }
     }
 }
